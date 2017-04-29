68c5257b978aa5c7a092bf1e2c17d47d2bc1a069
iniyan-kannappan <iniyan.kannappan@gmail.com>
2017-04-29 08:27:49 -0700

    correct move

--- Programs/game/chess/Chess.py
+++ Programs/game/chess/Chess.py
@@ -17,22 +17,37 @@ def getindex(coin):
         for column in range(0,8,1):
             if chessboard[row][column]==coin:
                 return(row,column)
-
+turn2='White'
 def move(turn):
+    global turn2
     print(turn+'s turn.Please enter the coin to move.')
     coin=input().upper()
-    position = getindex(coin)
+    position=getindex(coin)
     print(position)
+    i,j=getindex(coin)
     print('Enter a row.')
     row = int(input())
     print('Enter a column.')
     column = int(input())
-    chessboard[position[0]][position[1]] = '   '
-    chessboard[row][column] = coin
-    Board.drawboard(chessboard)
+    k=row
+    l=column
+    if (k==i-1 or k==i-2)and(l==j):
+        chessboard[position[0]][position[1]]='  '
+        chessboard[row][column] = coin
+        Board.drawboard(chessboard)
+        if turn2 == 'White':
+            turn2 = 'Black'
+        else:
+            turn2 = 'White'
+    else:
+        print('Wrong move!')
+
+
+
+
 while True:
-    move('White')
-    move('Black')
+    move(turn2)
+    move(turn2)
 #add useful comments to understand how the code works
 #add a conditition in the move that any pawn can move only 1 or 2 steps on the same column any other chess piece can continue to move
 #pawn could only move 2 steps in when in first row
