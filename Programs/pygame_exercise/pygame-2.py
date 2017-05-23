204435411618765a3d21dbdbdbc8268754d3d459
iniyan-kannappan <iniyan.kannappan@gmail.com>
2017-05-22 17:28:05 -0700

    checker board

--- Programs/pygame_exercise/pygame-2.py
+++ Programs/pygame_exercise/pygame-2.py
@@ -1,11 +1,18 @@
+import random
 import pygame
-def triangle():
-    pygame.init()
-    screen=pygame.display.set_mode((400,400))
-    color=(0,255,0)
-    pygame.draw.line(screen,color,(10,20),(40,20))
-    pygame.draw.line(screen,color,(10,20),(20,50))
-    pygame.draw.line(screen,color,(40,20),(20,50))
-    pygame.display.update()
+pygame.init()
+screen=pygame.display.set_mode((400,400))
+color=(0,255,0)
+randomnumber=random.randint(0,255),random.randint(0,255)
+def triangle(a,b,c):
 
-triangle()
+    pygame.draw.line(screen,color,a,b)
+    pygame.draw.line(screen,color,c,a)
+    pygame.draw.line(screen,color,b,c)
+    pygame.display.update()        
+for loop in range(0,10,1):
+    a=random.randint(0,400),random.randint(0,400)
+    b=random.randint(0,400),random.randint(0,400)
+    c=random.randint(0,400),random.randint(0,400) 
+    triangle(a,b,c)
+#homework:write a for loop to draw 10 random triangles
--- Programs/pygame_exercise/pygame-3.py
+++ Programs/pygame_exercise/pygame-3.py
@@ -1,17 +1,27 @@
 import random
 import time
 import pygame
+r=random.randint(0,255)
+g=random.randint(0,255)
+b=random.randint(0,255)
+pygame.init()
+screen=pygame.display.set_mode((255,255))
+color=(r,g,b)
+color2=[r,g,b]
 while True:
-    pygame.init()
-    screen=pygame.display.set_mode((255,255))
-    color=(0,255,0)
-    pygame.draw.circle(screen,color,(125,125),125)
-    pygame.display.update()
-    time.sleep(1)
+    for loop in range(5,135,10):    
+        
+        drawcircle=pygame.draw.circle(screen,color,(125,125),loop)
+        print(loop)
+        if drawcircle==255:
+            color.choice(color2)
+        pygame.display.update()
+        time.sleep(1)
+    for loop in range(125,0,-10):
+        screen.fill((0,0,0))
+        pygame.draw.circle(screen,color,(125,125),loop)
+        print(loop)
+        pygame.display.update()
+        time.sleep(1)
+
     
-    pygame.init()
-    screen2=pygame.display.set_mode((10,10))
-    color2=(255,255,255)
-    pygame.draw.circle(screen,color2,(5,5),5)
-    pygame.display.update()
-    time.sleep(1)
